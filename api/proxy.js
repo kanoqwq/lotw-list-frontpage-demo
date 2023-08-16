34fe3e41019a294f6e9ac1ef7ed13bd0923272a3
kanoqwq <kanoqwq@qq.com>
2023-08-17 02:41:57 +0800

    modfied:proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
     let target = ''
 
-    if (req.url.startsWith('/api')) {
+    if (req.url.startsWith('/lotw-api')) {
         // 这里填目标地址
         target = 'https://api.kanokano.cn/lotw-api'
     }
@@ -13,7 +13,7 @@ module.exports = (req, res) => {
         target,
         changeOrigin: true,
         pathRewrite: {
-            '^/api/': '/'
+            '^/lotw-api/': '/'
         }
     })(req, res)
 }
