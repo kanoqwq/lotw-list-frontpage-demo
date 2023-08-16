ef2c1f2b156a531a2ebbb632b1bde13f4325c5d7
kanoqwq <kanoqwq@qq.com>
2023-08-17 02:31:09 +0800

    feat:support vercel

---
+++ api/proxy.js
@@ -0,0 +1,19 @@
+// 该服务为 vercel serve跨域处理
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+    let target = ''
+
+    if (req.url.startsWith('/api')) {
+        // 这里填目标地址
+        target = 'https://api.kanokano.cn/lotw-api'
+    }
+    // 创建代理对象并转发请求
+    createProxyMiddleware({
+        target,
+        changeOrigin: true,
+        pathRewrite: {
+            '^/api/': '/'
+        }
+    })(req, res)
+}
