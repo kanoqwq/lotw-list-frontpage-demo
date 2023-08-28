fc25a4c49ae4ab9c29b32dde8bdb52e50fde89b1
kanoqwq <kanoqwq@qq.com>
2023-08-28 13:04:11 +0800

    modify: prettify the style

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import backgroundImage from './assets/images/background.jpg'
 const rootEle = document.getElementById('root')
-rootEle.style.background = `url(${backgroundImage})`
-rootEle.style.backgroundAttachment = 'fixed'
-rootEle.style.backgroundSize = 'cover'
+const body = document.body
+body.style.background = `url(${backgroundImage})`
+body.style.backgroundAttachment = 'fixed'
+body.style.backgroundSize = 'cover'
 const root = ReactDOM.createRoot(rootEle);
 root.render(
   <App />
